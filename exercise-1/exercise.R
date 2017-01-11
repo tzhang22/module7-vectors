1a5d8f18ad357086f42d80be9b5e027eea95c43b
Tony Zhang <tzhang22@uw.edu>
2017-01-10 16:41:32 -0800

    Completed exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,27 +1,28 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
-
+x <- 10:20
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
-
+y <- seq(21, 30)
 
 # Create a vector `z` by combining the vectors `x` and `y`
-
+z <- x + y
 
 # Create a variable `w` that contains the 11th element in your vector `z`
-
+w <- z[11]
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
-
+q <- c(z[2], z[3], z[4], z[5])
 
 # Create a variable `len` that is equal to the length of your vector `q`
-
+len <- length(q)
 
 ### Bonus ###
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-
+even <- seq(2, 100, 2)
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
+all(even %% 2 == 0)
